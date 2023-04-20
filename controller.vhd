84a0c7caa0766055e33689008358587a46cce736
AliAhmed0000 <76479231+AliAhmed0000@users.noreply.github.com>
2023-04-20 02:10:19 +0200

    Update controller.vhd

--- controller.vhd
+++ controller.vhd
@@ -6,7 +6,7 @@ USE IEEE.std_logic_unsigned.ALL;
 ENTITY controller IS
     PORT (
         opcode : IN STD_LOGIC_VECTOR(4 DOWNTO 0);
-        memWrite, memRead, wbEnable, aluEnable : OUT STD_LOGIC;
+        memWrite, memRead, wbEnable, aluEnable,inportControl : OUT STD_LOGIC;
         aluOperation : OUT STD_LOGIC_VECTOR(4 DOWNTO 0)
 
     );
@@ -22,40 +22,42 @@ BEGIN
             memRead <= '0';
             wbEnable <= '0';
             aluEnable <= '0';
-          
+            inportControl <='0';
 
         ELSIF (opcode = "00100") THEN --INC--
             memWrite <= '0';
             memRead <= '0';
             wbEnable <= '1';
             aluEnable <= '1';
+	        inportControl <='0';
           
         ELSIF (opcode = "01010") THEN --AND--
             memWrite <= '0';
             memRead <= '0';
             wbEnable <= '1';
             aluEnable <= '1';
-           
+            inportControl <='0';
 
         ELSIF (opcode = "01100") THEN --IN--
             memWrite <= '0';
             memRead <= '0';
             wbEnable <= '1';
             aluEnable <= '1';
-           
+            inportControl <='1';
 
         ELSIF (opcode = "10000") THEN --LDD--
             memWrite <= '0';
             memRead <= '1';
             wbEnable <= '1';
             aluEnable <= '1';
-            
+            inportControl <='0';
 
         ELSIF (opcode = "10001") THEN --STD--
             memWrite <= '1';
             memRead <= '0';
             wbEnable <= '0';
             aluEnable <= '1';
+	        inportControl <='0';
       
         END IF;
     END PROCESS;
