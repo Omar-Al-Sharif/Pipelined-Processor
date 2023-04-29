72b617e14f8994219e109f755b5cbb98d196b97d
ziad amr shalaby <zezoamr@yandex.com>
2023-04-29 06:44:53 +0300

    all done

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -22,6 +22,9 @@ def hex_to_bin(hexdec):
     # Pad the binary value with zeros if needed to reach 16 bits
     return binary.zfill(16)
 
+def int_to_bin(integer):
+    return bin(integer)[2:].zfill(16)
+
 def line_to_command(line, counter):
     
     opcode = '00000_'
@@ -79,7 +82,7 @@ def line_to_command(line, counter):
         imm_Value = hex_to_bin(line[2]) + '_'
     
     #ldm has OPCODE AND dest and src1 and immediate only 
-    elif line[0] == 'IADD ': 
+    elif line[0] == 'IADD': 
         opcode = opcode_dict[line[0]] 
         dest = register_dict[line[1]]
         src1 = register_dict[line[2]]
@@ -95,15 +98,17 @@ def line_to_command(line, counter):
         src2 = register_dict[line[2]]
         
     #IN AND JMP AND INC AND CALL AND JC AND JZ AND POP HAVE OPCODE AND DEST ONLY
-    elif line[0] == 'IN' or line[0] == 'JMP' or line[0] == 'INC' or line[0] == 'CALL' or line[0] == 'JC' or line[0] == 'JZ' or line[0] == 'POP':
+    elif line[0] == 'IN' or line[0] == 'JMP'  or line[0] == 'CALL' or line[0] == 'JC' or line[0] == 'JZ' or line[0] == 'POP':
         opcode = opcode_dict[line[0]]
         dest = register_dict[line[1]]
         
     #NOT AND DEC AND MOV AND LDD HAVE OPCODE AND DEST AND SRC1 ONLY
-    elif line[0] == 'NOT' or line[0] == 'DEC' or line[0] == 'MOV' or line[0] == 'LDD':
+    elif line[0] == 'NOT' or line[0] == 'DEC' or line[0] == 'INC' or line[0] == 'MOV' or line[0] == 'LDD':
         opcode = opcode_dict[line[0]]
         dest = register_dict[line[1]]
-        src1 = register_dict[line[2]]
+        try:
+            src1 = register_dict[line[2]]
+        except: src1 = register_dict[line[1]]
     
     #ADD AND SUB AND AND AND OR HAVE OPCODE AND DEST AND SRC1 AND SRC2 ONLY
     elif line[0] == 'ADD' or line[0] == 'SUB' or line[0] == 'AND' or line[0] == 'OR':
@@ -113,46 +118,70 @@ def line_to_command(line, counter):
         src2 = register_dict[line[3]]
     
     else:
-        print('instruction transalation error')
+        print('instruction transalation error', counter, opcode, dest, src1, src2, imm_Value)
+        for x in line:
+            print(x)
         
     return str(counter) + ": " + opcode + dest + src1 + src2 + imm_Value + uselessbits
     
     
-def work(inputfile, outputfile):
+def work(inputfile, outputfile, memfile):
+    instruction_write_dict = {}
+    mem_write_dict = []
+    
     with open(inputfile) as f, open(outputfile, 'w') as out:
         counter = 0
         for line in f:
             
-            if len(line.strip()) == 0 or line.strip()[0] == '#' : continue
-            result = process_line(line)
+            if len(line.strip()) == 0 or line.strip()[0] == '#': continue
             
-            if result[0].lower() == '.org': 
-                val = int(result[1], base=16) ## change hexadecimal val of org to int for processing
-                for x in range(val-counter):
-                    result = process_line('NOP')
-                    result = line_to_command(result, hex(counter)[2:])
-                    out.write(''.join(result) + '\n')
-                    counter +=1
+            result = process_line(line)
+            #print('result', result)
+            
+            if ( result[0].isdigit() or all(c in '0123456789abcdefABCDEF' for c in result[0]) ):
+                mem_write_dict.append( hex_to_bin( result[0] ) )
+            elif result[0].lower() == '.org': 
+                counter = int(result[1])
             else:    
                 result = line_to_command(result, hex(counter)[2:])
-                out.write(''.join(result) + '\n')
+                instruction_write_dict[counter] = result
                 counter +=1
-                print(result)
+                
         
-        val = counter
-        for x in range(2**16-val):
-            result = process_line('NOP')
-            result = line_to_command(result, hex(counter)[2:])
-            out.write(''.join(result) + '\n')
-            counter +=1
+        print('dict printing')
+        for key,val in instruction_write_dict.items():
+            print( key,val)
+                
+        for x in range(2**16):
+            if x in instruction_write_dict.keys():
+                out.write(''.join(instruction_write_dict[x]) + '\n')
+            else: 
+                result = process_line('NOP')
+                result = line_to_command(result, hex(x)[2:])
+                out.write(''.join(result) + '\n')
+                
+                
+    with open(memfile, 'w') as out:    
+        if len(mem_write_dict) != 0:
+            for x in range(2**16):
+                if x < len(mem_write_dict):
+                    out.write(''.join(hex(x)[2:] + ': ' + str(mem_write_dict[x])) + '\n')
+                else: 
+                    out.write(''.join(hex(x)[2:] + ': 0000000000000000') + '\n')    
+        else: print('use empty mem file')
             
 if __name__ == '__main__':
     work('D:\\projects\\Pipelined-Processor\\interpreter\\file.txt', 
-         'D:\\projects\\Pipelined-Processor\\interpreter\\anotherfile.txt')
+         'D:\\projects\\Pipelined-Processor\\interpreter\\anotherfile.txt', 'D:\\projects\\Pipelined-Processor\\interpreter\\placeholdermem.txt')
+    print('done')
     work('D:\\projects\\Pipelined-Processor\\interpreter\\orgtest.txt', 
-         'D:\\projects\\Pipelined-Processor\\interpreter\\orgtestoutput.txt')
+         'D:\\projects\\Pipelined-Processor\\interpreter\\orgtestoutput.txt', 'D:\\projects\\Pipelined-Processor\\interpreter\\placeholdermem.txt')
+    print('done')
     work('D:\\projects\\Pipelined-Processor\\interpreter\\codepart2.txt', 
-         'D:\\projects\\Pipelined-Processor\\interpreter\\codepart2translation.txt')
+         'D:\\projects\\Pipelined-Processor\\interpreter\\codepart2translation.txt', 'D:\\projects\\Pipelined-Processor\\interpreter\\codepart2mem.txt')
+    print('done')
+    
+    #testing funcs
     #print(hex_to_bin('a1'))
     #print(process_line('inc ax, bx, cx'))
     #print(line_to_command( ['PUSH', 'R1']  ))
