48b7a861f3601a18dc7eca0aaddc43002940bbaa
ziad amr shalaby <zezoamr@yandex.com>
2023-04-29 21:24:11 +0300

    Update interpreter.py

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -139,7 +139,7 @@ def work(inputfile, outputfile):
             
             if ( result[0].isdigit() or all(c in '0123456789abcdefABCDEF' for c in result[0]) ):
                 instruction_write_dict[counter] =  str(hex(counter)[2:]) + ": " + '11010_' + '000_' + '000_' + '000_' + hex_to_bin( result[0] ) + '_00'
-                #Reset opcode is 11010
+                #for Reset opcode is 11010
                 counter +=1
                 
             elif result[0].lower() == '.org': 
