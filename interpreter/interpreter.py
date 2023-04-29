9a67ed227122c62a8b53fd7d3bb6d4c32816636a
ziad amr shalaby <zezoamr@yandex.com>
2023-04-29 21:22:46 +0300

    Update interpreter.py

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -138,7 +138,8 @@ def work(inputfile, outputfile):
             #print('result', result)
             
             if ( result[0].isdigit() or all(c in '0123456789abcdefABCDEF' for c in result[0]) ):
-                instruction_write_dict[counter] =  str(hex(counter)[2:]) + ": " + '00000_' + '000_' + '000_' + '000_' + hex_to_bin( result[0] ) + '_00'
+                instruction_write_dict[counter] =  str(hex(counter)[2:]) + ": " + '11010_' + '000_' + '000_' + '000_' + hex_to_bin( result[0] ) + '_00'
+                #Reset opcode is 11010
                 counter +=1
                 
             elif result[0].lower() == '.org': 
